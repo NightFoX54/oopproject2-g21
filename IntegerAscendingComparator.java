ccd88c2c9d1741f127c76bba805c940ce1e25be6
Berkay Mustafa Arıkan <158609860+NightFoX54@users.noreply.github.com>
2024-11-27 13:22:43 +0300

    Add files via upload

---
+++ IntegerAscendingComparator.java
@@ -0,0 +1,9 @@
+import java.util.Comparator;
+
+public class IntegerAscendingComparator implements Comparator<Integer> {
+
+    @Override
+    public int compare(Integer i1, Integer i2) {
+        return i1 - i2;
+    }
+}
