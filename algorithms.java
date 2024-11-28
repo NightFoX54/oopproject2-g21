6b9e134b48deceef90dcbad33357ed5afc76d4e8
Berkay Mustafa Arıkan <158609860+NightFoX54@users.noreply.github.com>
2024-11-28 22:49:50 +0300

    Update algorithms.java

--- algorithms.java
+++ algorithms.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class algorithms{
 
-    public static void main(String[] args)
+    public static void algorithm()
     {  
         Scanner scanner = new Scanner(System.in);
         System.out.print("Type a number between 1000 and 10000 to declare the size of the array: ");
