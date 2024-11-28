3896c4f892ced6b98467b81f435de7ed3203f7e9
Meriç Ütkü <mericutku24@icloud.com>
2024-11-28 04:26:35 +0300

    Update manager.java
    added managerFire function

--- manager.java
+++ manager.java
@@ -1,7 +1,16 @@
+import java.util.*;
+import java.sql.*;
+import java.util.Scanner;
+
 class manager extends employee{
     public manager(String employee_id, String username, String role, String name, String surname, String phoneNo, String dateOfBirth, String dateOfStart, String email) {
         super(employee_id, username, role, name, surname, phoneNo, dateOfBirth, dateOfStart, email);
     }
+
+/* public static Connection connect() throws SQLException {
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/deneme", "root", "179492");
+    }
+*/
 
     public void displayAllEmployees() {
         final String query = "SELECT employee_id, username, name, surname, role, phoneNo, email FROM employees"; //SQL query for obtaining data from database.
@@ -14,10 +23,9 @@ class manager extends employee{
             ResultSetMetaData allEmps = res.getMetaData();
             int numOfCols = allEmps.getColumnCount();
 
-            //Table view for readibilty.
+            //Table view for better readibilty
             System.out.println("=====================================================================");
-            System.out.printf("%-15s%-20s%-20s%-20s%-20s%-20s%-20s\n");
-            System.out.printf("Employee ID", "Username", "Name", "Surname", "Role", "PhoneNo", "Email");
+            System.out.printf("%-15s%-20s%-20s%-20s%-20s%-20s%-20s\n", "Employee ID", "Username", "Name", "Surname", "Role", "PhoneNo", "Email");
             System.out.println("=====================================================================");
 
             // To view Query results:
@@ -31,4 +39,63 @@ class manager extends employee{
         } catch (SQLException e) {
             e.printStackTrace(); 
         } 
+    }
+
+    public void managerFire(){
+
+        System.out.println("Please enter the employee_id of the employee you want to fire:");
+        
+        Scanner scanner = new Scanner(System.in);
+        String employee_id = scanner.nextLine();
+
+        if(this.employee_id.equals(employee_id)){
+            System.out.println("The manager cannot fire himself/herself !");
+            return;
+        }
+
+        String selectQuery = "SELECT name, surname FROM employees WHERE employee_id = ?";
+        String name = null;
+        String surname = null;
+
+        // To obtain 'name' and 'surname' from database by using 'employee_id':
+        try {
+            PreparedStatement statementForNameSurname = connection.prepareStatement(selectQuery);
+            statementForNameSurname.setString(1, employee_id);
+            ResultSet res = statementForNameSurname.executeQuery();
+
+            
+            if (res.next()) {
+                name = res.getString("name");
+                surname = res.getString("surname");
+            } 
+            else {
+                System.out.println("No employee matched with Employee ID: " + employee_id);
+
+                return;
+            }
+        } 
+        catch (SQLException e) {
+            System.out.println("Error occured while obtaining employee details from database: " + e.getMessage());
+
+            return;
+        }
+
+        //Deleting employee from database depending on Managers' decision:
+        String deleteQuery = "DELETE FROM employees WHERE employee_id=? AND role!='manager' ";
+
+        try { 
+            PreparedStatement statementForDelete = connection.prepareStatement(deleteQuery);
+            statementForDelete.setString(1,employee_id);
+            int deletedRows = statementForDelete.executeUpdate();
+
+            if(deletedRows > 0){
+                System.out.println("Employee "+ name +" "+ surname +" "+ "has been fired !");
+            } 
+            else{
+                System.out.println("Employee with "+ employee_id + " " + "has not registered in the database !" );
+            }
+        }
+        catch (SQLException e) {
+            System.out.println("Some error ocureed when firing wanted employee: " + e.getMessage());
+        }
     }
