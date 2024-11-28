04b966985cf629449477c76fc2eda32f7f582c51
Berkay Mustafa Arıkan <158609860+NightFoX54@users.noreply.github.com>
2024-11-28 20:47:33 +0300

    Update authentication.java

--- authentication.java
+++ authentication.java
@@ -23,7 +23,7 @@ public class authentication {
                 String role = resultSet.getString("role");
                 String name = resultSet.getString("name");
                 String surname = resultSet.getString("surname");
-                String phone_no = resultSet.getString("phone_no");
+                String phone_no = resultSet.getString("phoneNo");
                 String dateof_birth = resultSet.getString("dateOfBirth");
                 String dateof_start = resultSet.getString("dateOfStart");
                 String email = resultSet.getString("email");
