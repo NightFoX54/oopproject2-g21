9d7450e507bdbd5a8360827e245ab1c29c3ed135
Berkay Mustafa Arıkan <158609860+NightFoX54@users.noreply.github.com>
2024-11-28 15:19:22 +0300

    Update regularEmployee.java

--- regularEmployee.java
+++ regularEmployee.java
@@ -1,5 +1,5 @@
 class regularEmployee extends employee{
-    public regularEmployee(String username, String role, String name, String surname, String phoneNo, String dateOfBirth, String dateOfStart, String email) {
-        super(username, role, name, surname, phoneNo, dateOfBirth, dateOfStart, email);
+    public regularEmployee(String employee_id, String username, String role, String name, String surname, String phoneNo, String dateOfBirth, String dateOfStart, String email) {
+        super(employee_id, username, role, name, surname, phoneNo, dateOfBirth, dateOfStart, email);
     }
 }
