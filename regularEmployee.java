d7fa4c481a848312f585e7c557ebf4d410bdea8b
ennes0 <mehmetenesoy@gmail.com>
2024-11-29 15:17:51 +0300

    Update regularEmployee.java

--- regularEmployee.java
+++ regularEmployee.java
@@ -3,3 +3,30 @@ class regularEmployee extends employee{
         super(employee_id, username, role, name, surname, phoneNo, dateOfBirth, dateOfStart, email);
     }
 }
+
+
+        public void regularEmployeeeMenu(){
+        start.clear();
+        System.out.println("Welcome " + this.name + " " + this.surname + "\n");
+        String operation = "";
+        while(!operation.equals("C")){
+            System.out.println("[A] Display Own Profile");
+            System.out.println("[B] Update Own Profile");
+            System.out.println("[C] Log Out.");
+            System.out.print("Choose the operation you want to do: ");
+            operation = start.scanner.nextLine();
+            operation = start.menuInput('C', operation, "Incorrect input! Type again to select the operation: ");
+            switch(operation){
+                case "A":
+                    this.displayProfile();
+                    break;
+                
+                case "B":
+                    this.updateProfile();
+                    break;  
+
+                case "C":
+                    break;
+            }
+        }
+    }
