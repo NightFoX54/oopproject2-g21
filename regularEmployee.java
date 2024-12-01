2f87a08d2bf9a0bd5870b9f9d2100fdac29dbcfa
ennes0 <mehmetenesoy@gmail.com>
2024-12-02 00:23:51 +0300

    Update regularEmployee.java

--- regularEmployee.java
+++ regularEmployee.java
@@ -1,10 +1,33 @@
+/**
+ * Represents the regular employee and its features which are uniqe for regular employee class
+ * It extends the {@code employee} class inherits certain properties
+ */
+
 class regularEmployee extends employee{
+    /**
+     * 
+     * @param employee_id the ID of the employee
+     * @param username the username of the employee
+     * @param role the specific role of the employee
+     * @param name the first name of the employee
+     * @param surname the surname of the employee
+     * @param phoneNo the phone number of the employee
+     * @param dateOfBirth the birth date of the employee
+     * @param dateOfStart the start date of the employee
+     * @param email the email of the employee
+     */
     public regularEmployee(String employee_id, String username, String role, String name, String surname, String phoneNo, String dateOfBirth, String dateOfStart, String email) {
         super(employee_id, username, role, name, surname, phoneNo, dateOfBirth, dateOfStart, email);
     }
 
-
-
+    /**
+     * The menu interface for regular employee 
+     * Menu displays: 
+     * <ul> Display the profile </li>
+     * <li> Update the current informations</li>
+     * <li> Log out options</li>
+     * The method loops until the employee selects the "Log Out" option.
+     */
     public void regularEmployeeeMenu(){
         String operation = "";
         while(!operation.equals("C")){
@@ -31,3 +54,4 @@ class regularEmployee extends employee{
         }
     }
 }
+
